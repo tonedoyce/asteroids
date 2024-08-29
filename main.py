c500b00de1159900239f67002ea3fc2b479e8e70
tonedoyce <doyce@live.de>
2024-08-29 21:13:44 +0200

    added clock and framerate

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0 				#time variable
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		clock.tick(60)
+		dt = clock.tick(60) / 1000
 
 		screen.fill("black", rect=None, special_flags=0)
 		pygame.display.flip()
